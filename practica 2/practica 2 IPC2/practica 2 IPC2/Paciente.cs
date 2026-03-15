62e913869deef68bba04d16f099b259eb3766143
Jonás-fg <3812317550101@ingenieria.usac.edu.gt>
2026-03-14 21:05:39 -0600

    [ADD] primer commit: creacion del proyecto en VS para la practica 2 de IPC2.

---
+++ practica 2/practica 2 IPC2/practica 2 IPC2/Paciente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_2_IPC2
+{
+    internal class Paciente
+    {
+    }
+}
